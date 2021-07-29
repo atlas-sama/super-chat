f804e864a65340f2624f9f299cef8d8e2bc96afc
atlas-sama <harshbhardwaj909@gmail.com>
2021-07-29 16:02:21 +0530

    Updates

---
+++ precache-manifest.5bb4ecd6ff36af5fe7835133ebe1e8cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2054a0d77d92efcb0ac4c155dda25b12",
+    "url": "/super-chat/index.html"
+  },
+  {
+    "revision": "46e0eb6bed47f8f8afce",
+    "url": "/super-chat/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "fca19a6eb7bc180c6961",
+    "url": "/super-chat/static/js/2.3d6c5704.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/super-chat/static/js/2.3d6c5704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e0eb6bed47f8f8afce",
+    "url": "/super-chat/static/js/main.02be3fab.chunk.js"
+  },
+  {
+    "revision": "cef6bd0c495a96cd6669",
+    "url": "/super-chat/static/js/runtime-main.17ee1584.js"
+  }
+]);
